44f200a1f3868ee7539a1a60f303939067288d92
Joel Alphonso <joel@locomotive.ca>
2021-08-24 11:00:47 -0400

    :art: refactor(GeoJSON): remove 'multi' GeoJSON types from GeoJSONGeometriesInterface.php because in the current context, it's unneeded

--- src/Charcoal/Property/GeoJSONGeometriesInterface.php
+++ src/Charcoal/Property/GeoJSONGeometriesInterface.php
@@ -15,17 +15,11 @@ interface GeoJSONGeometriesInterface
     public const POINT             = 'point';
     public const LINE_STRING       = 'lineString';
     public const POLYGON           = 'polygon';
-    public const MULTI_POINT       = 'multiPoint';
-    public const MULTI_LINE_STRING = 'multiLineString';
-    public const MULTI_POLYGON     = 'multiPolygon';
 
     // MySQL naming conventions
     public const MYSQL_POINT               = 'POINT';
     public const MYSQL_LINE_STRING         = 'LINESTRING';
     public const MYSQL_POLYGON             = 'POLYGON';
-    public const MYSQL_MULTI_POINT         = 'MULTIPOINT';
-    public const MYSQL_MULTI_LINE_STRING   = 'MULTILINESTRING';
-    public const MYSQL_MULTI_POLYGON       = 'MULTIPOLYGON';
     public const MYSQL_GEOMETRY_COLLECTION = 'GEOMETRYCOLLECTION';
 
     /**
@@ -37,9 +31,6 @@ interface GeoJSONGeometriesInterface
         self::POINT,
         self::LINE_STRING,
         self::POLYGON,
-        self::MULTI_POINT,
-        self::MULTI_LINE_STRING,
-        self::MULTI_POLYGON,
     ];
 
     /**
@@ -51,9 +42,6 @@ interface GeoJSONGeometriesInterface
         self::POINT             => self::MYSQL_POINT,
         self::LINE_STRING       => self::MYSQL_LINE_STRING,
         self::POLYGON           => self::MYSQL_POLYGON,
-        self::MULTI_POINT       => self::MYSQL_MULTI_POINT,
-        self::MULTI_LINE_STRING => self::MYSQL_MULTI_LINE_STRING,
-        self::MULTI_POLYGON     => self::MYSQL_MULTI_POLYGON,
     ];
 
     /**
